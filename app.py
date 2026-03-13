176faaa146739355d6a91f5b2ed4e45cb0c56955
rosagabriela <rosagabrielapina@gmail.com>
2026-03-13 08:35:24 -0700

    se agregaron nuevas rutas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,9 +7,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+
+
+@app.route('/api/users', methods=["POST"])
+def create_user():
+    data = request.get_json()
+    name = data.get("name")
+    email = data.get("email")
+    password = data.get("password")
+
+    print(f"Nombre: {name}")
+    print(f"Email:{email}")
+
+    return jsonify({"success":True})
+
 
 # undersocre methods and properties
 if __name__ == '__main__':
