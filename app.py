bdbf5772915a8e4401adc4e1136a6b3f13221234
rosagabriela <rosagabrielapina@gmail.com>
2026-04-16 18:31:27 -0700

    Actualiza ruta welcome

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from entities.user import User
+from entities.account import Account
+from flask_login import LoginManager, login_user, login_required, logout_user,  current_user
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "index"
 
 
 @app.route('/')
@@ -8,29 +20,68 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/signup')
+@app.route("/signup")
 def signup():
-    return render_template('signup.html')
+    return render_template("signup.html")
 
 
 @app.route('/welcome')
+@login_required
 def welcome():
-    return render_template('welcome.html')
+    account = Account.get_account_by_user(current_user.id)
+    return render_template('welcome.html', account=account)
 
 
 @app.route('/api/users', methods=["POST"])
 def create_user():
     data = request.get_json()
+
     name = data.get("name")
     email = data.get("email")
     password = data.get("password")
 
-    print(f"Nombre: {name}")
-    print(f"Email:{email}")
+    if User.check_email_exists(email):
+        return jsonify({"success": False, "message": "El correo electrónico ingresado ya se encuentra registrado."}), 409
 
-    return jsonify({"success":True})
+    if User.save(name, email, password):
+        return jsonify({"success": True, "message": "Su cuenta fue creada correctamente."}), 201
+    else:
+        return jsonify({"success": False, "message": "Ocurrió un error al crear su cuenta. Intente de nuevo"}), 500
 
 
-# undersocre methods and properties
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.check_login(email, password)
+    if user:
+
+        login_user(user)
+        return jsonify({
+            "success": True,
+            "message": "Sesión iniciada correctamente"
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Los datos de acceso ingresados no son correctos."
+        }), 401
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(user_id)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
 if __name__ == '__main__':
     app.run()
